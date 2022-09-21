bc0afb6b86081d11edef7d22a5f2080f8a033b8c
Brian Orsua <brianorsua@gmail.com>
2022-09-20 21:59:02 -0700

    Take args of val and output to new array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...val) {
+  let a = [];
+//take all arguments of ...val and output to a
+  for (const args of val) {
+    a.push(args);
+  } 
+//pass each value of new array and remove from arr
 
+for (let i = 0; i < a.length; i++){
+if (arr.indexOf(a[i])) {
+    let index = arr.indexOf(a[i]);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  } 
+} 
+  return arr;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
